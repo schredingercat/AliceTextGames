1fcbc2f13535457a6b54a83f32ccc4c817aa75b7
Богомолов Илья <schredinger@mail.ru>
2019-01-19 13:31:42 +0700

    Функционал взаимодействия с API Алисы вынесен в отдельный проект

--- AliceTextGames/AliceApi/Modules/Request.cs
+++ AliceTextGames/AliceApi/Modules/Request.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace WebApi.Models
+namespace AliceApi
 {
     public class Request
     {
--- AliceTextGames/WebApi/Controllers/AliceController.cs
+++ AliceTextGames/WebApi/Controllers/AliceController.cs
@@ -3,8 +3,8 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AliceApi;
 using Microsoft.AspNetCore.Mvc;
-using WebApi.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
